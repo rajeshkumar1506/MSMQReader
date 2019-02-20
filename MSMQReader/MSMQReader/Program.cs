88ef4c454c0668fb614a4613ed17eb3ad8122b48
rajeshkumar1506 <rajeshkumar1506@gmail.com>
2019-02-20 10:23:48 -0500

    Add files via upload

---
+++ MSMQReader/MSMQReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Messaging;
+using System.Configuration;
+
+namespace MSMQReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = ConfigurationManager.AppSettings["MsmqPath"];
+            var lstMessage = ReadQue(path);
+        }
+
+        private static List<string> ReadQue(string path)
+        {
+            MessageQueue messageQueue = null;
+            var listMessage = new List<string>();
+            try
+            {
+                using (messageQueue = new MessageQueue(path))
+                {
+                    var messages = messageQueue.GetAllMessages();
+                    foreach (var message in messages)
+                    {
+                        message.Formatter = new XmlMessageFormatter(new Type[] { typeof(Model) });
+                        var msg = (Model)message.Body;
+                    }
+                }
+                return listMessage;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            finally
+            {
+                messageQueue.Dispose();
+            }
+        }
+    }
+}
